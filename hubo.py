986ed02b44afe3f7d1d519595859a23c77a65b71
Juan Lee <juanlee@kaist.ac.kr>
2019-11-02 16:35:30 +0900

    Add cs101 files

---
+++ hubo.py
@@ -0,0 +1,9 @@
+from cs1robots import *
+
+load_world('worlds/world.wld')
+# edit_world()
+
+hubo1 = Robot(avenue=2, street=2)
+hubo2 = Robot(avenue=8, street=2, orientation="W")
+
+# save_world("./worlds/world.wld")
